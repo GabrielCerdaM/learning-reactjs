155c6a958bfd00a4671901c12ebc4d08e921c2d0
Gabriel Cerda <gabriel.cerda.m@gmail.com>
2021-04-03 21:15:19 -0300

    try yo push main

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { TodoApp } from './components/08-useReducer/TodoApp';
-// import { Padre } from './components/07-tarea-memo/Padre';
 
 ReactDOM.render(<TodoApp />,  document.getElementById('root'));
-
-// import './components/08-useReducer/intro-reducer'
